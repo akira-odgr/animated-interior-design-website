8dfebd0f64d7de38568069dc35735e86a4dd2c42
akira-odgr <177723350+akira-odgr@users.noreply.github.com>
2026-02-13 00:21:59 +0900

    feat:typesに新たに型の設定をしました。

--- types/types.ts
+++ types/types.ts
@@ -3,3 +3,42 @@ export type NavItem = {
     label: string;
     href: string;
 };
+
+export type StatusItem = {
+    id: number;
+    value: string;
+    label: string;
+};
+
+export type ServiceItem = {
+    id: number;
+    img: string;
+    title: string;
+};
+
+export type ProjectItem = {
+    id: number;
+    img: string;
+    title: string;
+    desc: string;
+};
+
+export type ProcessItem = {
+    id: number;
+    title: string;
+    step: string;
+    text: string;
+};
+
+export type BlogItem = {
+    id: number;
+    title: string;
+    text: string;
+    date: string;
+};
+
+export type FooterItem = {
+    id: number;
+    title: string;
+    list: string[];
+};
